b86365ef41a94d635e67439cdd15a65f3b372218
Cubed <47624167+cubed-guy@users.noreply.github.com>
2022-08-21 11:12:55 +0530

    Fixed comparison bug
    Ice can correctly compute up to 94 Fibonacci numbers (starting from 0)

--- Examples/While Demo.asm
+++ Examples/While Demo.asm
@@ -7,34 +7,30 @@ global main
 segment .bss
 $x: resb 1
 $i: resb 1
-$j: resb 1
-$b: resb 4
-$a: resb 4
-$c: resb 4
+$b: resb 8
+$a: resb 8
+$c: resb 8
 
 segment .data
 _p: db `%u\n`, 0
 _c: db 0, 0
 _ln: db 0, 10, 0
 _fmt_udstr: db `%llu`, 0
-_str: times 3 dq 0
+_str: dq 0, 0, 0
 _p_str: db `%s`, 0
 _p_strln: db `%s\n`, 0
-$space: db ` 0`
+$space: db 32, 0
 
 segment .text
 main:
-
-;7: 3x = 0
 mov rax, 0
 mov byte [$x],  al
 
 .L0:
-
-;8: while x < 100
 mov  al, byte [$x]
 mov rcx, rax
 mov rax, 100
+xor rbx, rbx
 mov  bl,  cl
 cmp rbx, rax
 setl al
@@ -43,7 +39,6 @@ setnz al
 test al, al
 jz .L0_end
 
-;9: x = x + 1
 mov  al, byte [$x]
 mov rcx, rax
 mov rax, 1
@@ -52,8 +47,6 @@ add rbx, rax
 mov rax, rbx
 mov byte [$x],  al
 
-
-;10: println(x)
 mov rcx, _str
 mov rdx, _fmt_udstr
 xor r8, r8
@@ -70,37 +63,23 @@ pop rbp
 jmp .L0
 .L0_end:
 
-;16: space[1] = 0
-mov rax, 0
-xor rbx, rbx
-mov rbx, 1
-mov  al,   al
-mov byte [rbx * 1 + $space],  al
-
-
-;17: println(space)
 mov rax, $space
 mov rdx, rax
 mov rcx, _p_strln
 push rbp
 call printf
 pop rbp
-
-;24: 3i = 0
 mov rax, 0
 mov byte [$i],  al
 
-
-;26: 5b = 1
 mov rax, 1
-mov dword [$b], eax
+mov qword [$b], rax
 
 .L1:
-
-;28: while i < 10
 mov  al, byte [$i]
 mov rcx, rax
-mov rax, 10
+mov rax, 94
+xor rbx, rbx
 mov  bl,  cl
 cmp rbx, rax
 setl al
@@ -109,17 +88,14 @@ setnz al
 test al, al
 jz .L1_end
 
-;29: c = a+b
-mov eax, dword [$a]
+mov rax, qword [$a]
 mov rcx, rax
-mov eax, dword [$b]
-mov ebx, ecx
-add ebx, eax
+mov rax, qword [$b]
+mov rbx, rcx
+add rbx, rax
 mov rax, rbx
-mov dword [$c], eax
+mov qword [$c], rax
 
-
-;30: print(i)
 mov rcx, _str
 mov rdx, _fmt_udstr
 xor r8, r8
@@ -133,20 +109,16 @@ mov rcx, _p_str
 push rbp
 call printf
 pop rbp
-
-;31: print(space)
 mov rax, $space
 mov rdx, rax
 mov rcx, _p_str
 push rbp
 call printf
 pop rbp
-
-;32: println(a)
 mov rcx, _str
 mov rdx, _fmt_udstr
 xor r8, r8
-mov r8d, dword [$a]
+mov r8, qword [$a]
 push rbp
 call sprintf
 pop rbp
@@ -156,18 +128,12 @@ mov rcx, _p_strln
 push rbp
 call printf
 pop rbp
+mov rax, qword [$b]
+mov qword [$a], rax
 
-;33: a = b
-mov eax, dword [$b]
-mov dword [$a], eax
+mov rax, qword [$c]
+mov qword [$b], rax
 
-
-;34: b = c
-mov eax, dword [$c]
-mov dword [$b], eax
-
-
-;35: i = i + 1
 mov  al, byte [$i]
 mov rcx, rax
 mov rax, 1
@@ -178,6 +144,7 @@ mov byte [$i],  al
 
 jmp .L1
 .L1_end:
+
 xor rcx, rcx
 push rbp
 call exit
--- Examples/While Demo.ice
+++ Examples/While Demo.ice
@@ -12,20 +12,18 @@ endwhile
 
 # String literals are only supported for initialization
 # That's why we declare a variable to use the string
-@[]3 space = " 0"
-space[1] = 0  # strings don't null terminate by default yet
+@[]3 space = " \x00"   # strings don't null terminate by default yet
 println(space)  # print() and println() call printf under the hood
 # and printf only works on null ending strings
 
 # All these quirks will be fixed in the future, don't worry :)
 
 
-# Demo 2: print the first 10 fibonacci numbers
+# Demo 2: print the first 25 fibonacci numbers
 3i = 0
-3j
-5b = 1
-5a 5c # everything is zero initialized by default
-while i < 10
+6b = 1
+@6 a c # everything is zero initialized by default
+while i < 94
 	c = a+b
 	print(i)
 	print(space)
