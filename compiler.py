5ee83343744d04dbb289394a5bbe553225c70472
Cubed <47624167+cubed-guy@users.noreply.github.com>
2022-11-24 20:24:45 +0530

    Extracted passes into methods for testing

--- compiler.py
+++ compiler.py
@@ -35,7 +35,7 @@ variables = {}
 if Shared.debug:
 	Shared.line_no = 0
 	Shared.line = '[DEBUG] *Empty line*'
-	print('BUILTINS: ', *snippets.snippets)
+	# print('BUILTINS: ', *snippets.snippets)
 
 	class Debug:
 		@staticmethod
@@ -56,7 +56,8 @@ if Shared.debug:
 			snippets.read_snippets(sfile, new_crlf)
 		@staticmethod
 		def set_output_file(new_outfile):
-			global output
+			global output, out
+			out = new_outfile
 			def output(*args, file = new_outfile, **kwargs):
 				print(*args, **kwargs, file = file)
 			snippets.set_output(output)
@@ -260,274 +261,280 @@ def dedent(indent_stack, branch_stack, ladder_stack, end = True):
 		snippets.insert('_if_end', (ladder_stack.pop(), branch_id))
 	if Shared.debug: print(f'DEDENT: {ladder_stack = }, {branch_stack = }')
 
-if __name__ == '__main__':
-	snippets.insert('_header')
+class passes:
+	@staticmethod
+	def declaration():
+		# Writing to bss segment
 
-	# Writing to bss segment
+		infile.seek(0)
+		output('\nsegment .bss')
+		for Shared.line_no, Shared.line in enumerate(infile, 1):
+			stmt = Patterns.stmt.match(Shared.line)[0].strip()
+			if Patterns.keywords.match(stmt): continue
 
-	output('\nsegment .bss')
-	for Shared.line_no, Shared.line in enumerate(infile, 1):
-		stmt = Patterns.stmt.match(Shared.line)[0].strip()
-		if Patterns.keywords.match(stmt): continue
+			lhs, *rhs = Patterns.equal.split(stmt, maxsplit = 1)
+			lhs = lhs.strip()
+			decls = lhs.split()
+			decl = Patterns.decl.match(lhs)
 
-		lhs, *rhs = Patterns.equal.split(stmt, maxsplit = 1)
-		lhs = lhs.strip()
-		decls = lhs.split()
-		decl = Patterns.decl.match(lhs)
+			if not decl: continue
 
-		if not decl: continue
-
-		if not rhs:
-			if decl[2]:
-				label = lhs.split()[0][1:]
-				for decl in decls[1:]:
-					decl = decl.strip()
-					declare(label, decl)
-			else:
-				for decl in decls:
-					decl = decl.strip()
-					match = Patterns.decl.match(decl)
-					if not match:
-						err(f'SyntaxError: Expected a declaration token.')
-					label, name = match[1].strip(), match[3]
-					declare(label, name)
-			continue
-
-		if not lhs: err('SyntaxError: Assignment without destination.')
-		if len(decls) != 1 and not (decl[2] and len(decls) == 2):
-			err('SyntaxError: Assignment with multiple declarations')
-		var = decl[0]
-		size = labels.element_size(decl[1])
-
-		rhs = rhs[0].strip()
-		end = False
-		# Clean this up. Must expect comma after each value.
-		if rhs[0] == '[': # TODO: robust syntax
-			init = bytearray()
-			for token in Patterns.wsep.split(rhs[1:]):
-				token = token.strip()
-				if not token: continue
-				if end:
-					err('SyntaxError: Token found after array initialisation.')
-				if token.isdigit():
-					init.extend(int(token).to_bytes(size, 'big'))
-				elif token == ']': end = True
-				elif token != ',': err('SyntaxError: Invalid token'
-					f' {token!r} in array initialisation.')
-			if not end:
-				err('SyntaxError: Multi-line arrays are not yet supported.')
-
-		elif rhs[0] in '"\'': # single char single quotes not yet char literal
-			# TODO: test robust syntax
-			init = bytearray()
-			s = rhs[0]
-			str_state = CHAR
-			for c in rhs[1:]:
-				if end:
-					err('SyntaxError: Token found after string initialisation.')
-				elif str_state == ESCAPE:
-					str_state = CHAR
-					if c == 'x': str_state = HEX_ESCAPE; init.append(0)
-					elif c not in escape_sequences:
-						err(f'SyntaxError: Invalid escape character {c!r}.')
-					else: init.append(ord(escape_sequences[c]))
-				elif str_state == HEX_ESCAPE:
-					if not Patterns.hex.match(c):
-						err('SyntaxError: Expected hexadecimal character.')
-
-					if not init[-1]: init[-1] |= int(c, 16)<<4 | 15
-					else:
-						init[-1] = ~15&init[-1] | int(c, 16)
-						str_state = CHAR
-
-				elif c == '\\': str_state = ESCAPE
-				elif c == s: end = True
-				else: init.append(ord(c))
-			else:
-				if not end: err('SyntaxError: EOL while parsing string.')
-		else: init = None
-
-		var = var.strip()
-		match = Patterns.decl.match(var)
-		if not match: err(f'SyntaxError: Expected a declaration token.')
-		name = match[3]
-		label = match[2] or match[1]
-		declare(label, name, init = init)
-
-	# Writing to the data segment
-
-	if Shared.debug: print('VARIABLES:', *variables.values())
-
-	output()
-	snippets.insert('_data')
-	if Shared.debug: print('\nINITS:'); inits = False
-	for var in variables.values():
-		if not var.init: continue
-		if Shared.debug: print(var.name, '=', var.init); inits = True
-
-		output(var.enc_name, end = ': db ')
-		out = repr(var.init)[12:-2].replace('`', '\\`')
-		if '\\x' not in out: output(f'`{out}`')
-		else: output(*var.init, sep = ', ')
-	if Shared.debug and not inits: print(None)
-	if Shared.debug: print()
-
-	# Writing to the text segment
-
-	next_ladder_id = 0
-	ladder_stack = []
-	branch_stack = []
-
-	indent_stack = ['']
-	prev_indent  = ''
-	expect_indent = False
-
-	infile.seek(0)
-	output('\nsegment .text')
-	output('main:')
-	for Shared.line_no, Shared.line in enumerate(infile, 1):
-		match = Patterns.stmt.match(Shared.line)
-		if not match: err('SyntaxError: EOL in string')  # only possibility?
-
-		curr_indent, Shared.line = match[1], match[2]
-		if not Shared.line.rstrip(): continue
-		if Shared.debug: print('\n%d: %r' % (Shared.line_no, Shared.line))
-
-		kw = Patterns.keywords.match(Shared.line)
-
-		if expect_indent:
-			if (curr_indent == prev_indent
-				or not curr_indent.startswith(prev_indent)):
-				err('IndentationError: Expected indent block.')
-			indent_stack.append(curr_indent)
-			expect_indent = False
-		elif curr_indent != prev_indent:
-			for dedents, indent in enumerate(reversed(indent_stack), 0):
-				if curr_indent == indent: break
-			else: err('IndentationError: '
-				'Current indentation does not match any outer indentation')
-
-			if Shared.debug: print('Multiple dedents:',
-				f'{len(branch_stack) = }',
-				f'{len(ladder_stack) = }',
-				f'{dedents = }',
-			)
-			for i in range(dedents-1):
-				dedent(indent_stack, branch_stack, ladder_stack)
-			if Shared.debug: print('One more dedent',
-				f'{len(branch_stack) = }',
-				f'{len(ladder_stack) = }',
-			)
-			dedent(indent_stack, branch_stack, ladder_stack,
-				end = not (kw and kw[1] in elses))
-
-		if Shared.debug: print(f'INDENT UPDATE: {indent_stack = } '
-			f' from {prev_indent!r} to {curr_indent!r}')
-		prev_indent = curr_indent
-
-
-		if kw is None:
-			dest, _, exp = Shared.line.rpartition('=')
-			dest = dest.strip()
-			exp  = exp.strip()
-
-			if Shared.debug: print(f'{Shared.line!r}',
-				f'-> exp: {Patterns.decl.match(exp)},'
-				f'dest: {Patterns.decl.match(dest)}')
-			
-			# no destination, only declaration
-			decl = Patterns.decl.match(exp)
-			if not dest and decl:
-				if Shared.debug: print('Exp declaration')
-				label = decl[2] or decl[1]
-				# accepts multi non-@ decl
-				for var in exp.split()[1:]: get_var(var).set_label(label)
+			if not rhs:
+				if decl[2]:
+					label = lhs.split()[0][1:]
+					for decl in decls[1:]:
+						decl = decl.strip()
+						declare(label, decl)
+				else:
+					for decl in decls:
+						decl = decl.strip()
+						match = Patterns.decl.match(decl)
+						if not match:
+							err(f'SyntaxError: Expected a declaration token.')
+						label, name = match[1].strip(), match[3]
+						declare(label, name)
 				continue
 
-			# (re)declaration with assignment
-			decl = Patterns.decl.match(dest)
-			if decl:
-				if Shared.debug: print('Dest declaration')
-				label = decl[2] or decl[1]
-				var = get_var(decl[3])
-				var.set_label(label)
+			if not lhs: err('SyntaxError: Assignment without destination.')
+			if len(decls) != 1 and not (decl[2] and len(decls) == 2):
+				err('SyntaxError: Assignment with multiple declarations')
+			var = decl[0]
+			size = labels.element_size(decl[1])
 
-			if not exp or exp[0] not in '["\'': parse(exp)
-			elif decl: dest = '' # sequence literals only initialise right now
-			else: err('SyntaxError: Sequence literals '
-				'not yet supported outside declaration.')
+			rhs = rhs[0].strip()
+			end = False
+			# Clean this up. Must expect comma after each value.
+			if rhs[0] == '[': # TODO: robust syntax
+				init = bytearray()
+				for token in Patterns.wsep.split(rhs[1:]):
+					token = token.strip()
+					if not token: continue
+					if end:
+						err('SyntaxError: Token found after array initialisation.')
+					if token.isdigit():
+						init.extend(int(token).to_bytes(size, 'big'))
+					elif token == ']': end = True
+					elif token != ',': err('SyntaxError: Invalid token'
+						f' {token!r} in array initialisation.')
+				if not end:
+					err('SyntaxError: Multi-line arrays are not yet supported.')
 
-			# for no op (and for assignment also maybe?) check `label is None`
-			if dest == '': continue
-			elif not exp: err('SyntaxError: Expected an expression.')
-			else: assign(dest) # TODO: optimize redundant `mov rax` assign(dest, var)
-			continue
+			elif rhs[0] in '"\'': # single char single quotes not yet char literal
+				# TODO: test robust syntax
+				init = bytearray()
+				s = rhs[0]
+				str_state = CHAR
+				for c in rhs[1:]:
+					if end:
+						err('SyntaxError: Token found after string initialisation.')
+					elif str_state == ESCAPE:
+						str_state = CHAR
+						if c == 'x': str_state = HEX_ESCAPE; init.append(0)
+						elif c not in escape_sequences:
+							err(f'SyntaxError: Invalid escape character {c!r}.')
+						else: init.append(ord(escape_sequences[c]))
+					elif str_state == HEX_ESCAPE:
+						if not Patterns.hex.match(c):
+							err('SyntaxError: Expected hexadecimal character.')
 
-		# Keyword Statements
+						if not init[-1]: init[-1] |= int(c, 16)<<4 | 15
+						else:
+							init[-1] = ~15&init[-1] | int(c, 16)
+							str_state = CHAR
 
-		if   kw[1] == 'while':
-			if not kw[2]: err('SyntaxError: no condition given in while')
-			ladder_id = Literal('3', str(next_ladder_id))
-			snippets.insert('_while_precond', (ladder_id,))
-			size_n = parse(kw[2])
-			snippets.insert('_while_postcond',
-				(ladder_id, Register(str(size_n), 'a')))
-			ladder_stack.append(ladder_id)
-			branch_stack.append(WHILE_BRANCH)
-			next_ladder_id += 1
-			if Shared.debug:
-				print(f'while: {ladder_stack = }, {branch_stack = }')
-		elif kw[1] == 'if':
-			if not kw[2]: err('SyntaxError: no condition given in if')
-			ladder_id = Literal('3', str(next_ladder_id))
-			branch_id = Literal('3', '0')
-			size_n = parse(kw[2])
-			snippets.insert('_if',(
-				ladder_id, branch_id, Register(str(size_n), 'a')))
-			ladder_stack.append(ladder_id)
-			branch_stack.append(branch_id)
-			next_ladder_id += 1
-			if Shared.debug:
-				print(f'if:    {ladder_stack = }, {branch_stack = }')
-		elif kw[1] == 'elif':
-			ladder_id = ladder_stack[-1]
-			branch_id = branch_stack[-1]
-			if branch_id is WHILE_BRANCH:
-				err('SyntaxError: elif not allowed after while')
-			snippets.insert('_else', (ladder_id, branch_id))
-			size_n = parse(kw[2])
-			branch_id.name = str(int(branch_id.name)+1)
-			snippets.insert('_if', (
-				ladder_id, branch_id, Register(str(size_n), 'a')))
-			if Shared.debug:
-				print(f'elif:  {ladder_stack = }, {branch_stack = }')
-		elif kw[1] == 'else':
-			if kw[2]: err('SyntaxError: else takes no expression')
-			ladder_id = ladder_stack[-1]
-			branch_id = branch_stack[-1]
-			if branch_id is WHILE_BRANCH:
-				err('SyntaxError: else not yet supported after while')
-				snippets.insert('_while_else', (ladder_id,))
-			# else: snippets.insert('_if_else', (ladder_id, branch_id))
-			else: snippets.insert('_else', (ladder_id, branch_id))
-			branch_id.name = str(int(branch_id.name)+1)
-			if Shared.debug:
-				print(f'else:  {ladder_stack = }, {branch_stack = }')
-		else:
-			# return, break, continue etc. those that don't end with ':'
-			...
-			output()
-			continue
-		if not kw[3]: err(f'SyntaxError: Colon required at the end of {kw[1]}')
-		expect_indent = True
+					elif c == '\\': str_state = ESCAPE
+					elif c == s: end = True
+					else: init.append(ord(c))
+				else:
+					if not end: err('SyntaxError: EOL while parsing string.')
+			else: init = None
+
+			var = var.strip()
+			match = Patterns.decl.match(var)
+			if not match: err(f'SyntaxError: Expected a declaration token.')
+			name = match[3]
+			label = match[2] or match[1]
+			declare(label, name, init = init)
+
+		# Writing to the data segment
+
+		if Shared.debug: print('VARIABLES:', *variables.values())
 
 		output()
+		snippets.insert('_data')
+		if Shared.debug: print('\nINITS:'); inits = False
+		for var in variables.values():
+			if not var.init: continue
+			if Shared.debug: print(var.name, '=', var.init); inits = True
 
-	if expect_indent:
-		err('IndentationError: Expected indentation, got EOF instead.')
-	while len(indent_stack) > 1:
-		dedent(indent_stack, branch_stack, ladder_stack)
+			output(var.enc_name, end = ': db ')
+			out = repr(var.init)[12:-2].replace('`', '\\`')
+			if '\\x' not in out: output(f'`{out}`')
+			else: output(*var.init, sep = ', ')
+		if Shared.debug and not inits: print(None)
+		if Shared.debug: print()
 
+	def codegen():
+		# Writing to the text segment
+
+		next_ladder_id = 0
+		ladder_stack = []
+		branch_stack = []
+
+		indent_stack = ['']
+		prev_indent  = ''
+		expect_indent = False
+
+		infile.seek(0)
+		output('\nsegment .text')
+		output('main:')
+		for Shared.line_no, Shared.line in enumerate(infile, 1):
+			match = Patterns.stmt.match(Shared.line)
+			if not match: err('SyntaxError: EOL in string')  # only possibility?
+
+			curr_indent, Shared.line = match[1], match[2]
+			if not Shared.line.rstrip(): continue
+			if Shared.debug: print('\n%d: %r' % (Shared.line_no, Shared.line))
+
+			kw = Patterns.keywords.match(Shared.line)
+
+			if expect_indent:
+				if (curr_indent == prev_indent
+					or not curr_indent.startswith(prev_indent)):
+					err('IndentationError: Expected indent block.')
+				indent_stack.append(curr_indent)
+				expect_indent = False
+			elif curr_indent != prev_indent:
+				for dedents, indent in enumerate(reversed(indent_stack), 0):
+					if curr_indent == indent: break
+				else: err('IndentationError: '
+					'Current indentation does not match any outer indentation')
+
+				if Shared.debug: print('Multiple dedents:',
+					f'{len(branch_stack) = }',
+					f'{len(ladder_stack) = }',
+					f'{dedents = }',
+				)
+				for i in range(dedents-1):
+					dedent(indent_stack, branch_stack, ladder_stack)
+				if Shared.debug: print('One more dedent',
+					f'{len(branch_stack) = }',
+					f'{len(ladder_stack) = }',
+				)
+				dedent(indent_stack, branch_stack, ladder_stack,
+					end = not (kw and kw[1] in elses))
+
+			if Shared.debug: print(f'INDENT UPDATE: {indent_stack = } '
+				f' from {prev_indent!r} to {curr_indent!r}')
+			prev_indent = curr_indent
+
+
+			if kw is None:
+				dest, _, exp = Shared.line.rpartition('=')
+				dest = dest.strip()
+				exp  = exp.strip()
+
+				if Shared.debug: print(f'{Shared.line!r}',
+					f'-> exp: {Patterns.decl.match(exp)},'
+					f'dest: {Patterns.decl.match(dest)}')
+				
+				# no destination, only declaration
+				decl = Patterns.decl.match(exp)
+				if not dest and decl:
+					if Shared.debug: print('Exp declaration')
+					label = decl[2] or decl[1]
+					# accepts multi non-@ decl
+					for var in exp.split()[1:]: get_var(var).set_label(label)
+					continue
+
+				# (re)declaration with assignment
+				decl = Patterns.decl.match(dest)
+				if decl:
+					if Shared.debug: print('Dest declaration')
+					label = decl[2] or decl[1]
+					var = get_var(decl[3])
+					var.set_label(label)
+
+				if not exp or exp[0] not in '["\'': parse(exp)
+				elif decl: dest = '' # sequence literals only initialise right now
+				else: err('SyntaxError: Sequence literals '
+					'not yet supported outside declaration.')
+
+				# for no op (and for assignment also maybe?) check `label is None`
+				if dest == '': continue
+				elif not exp: err('SyntaxError: Expected an expression.')
+				else: assign(dest) # TODO: optimize redundant `mov rax` assign(dest, var)
+				continue
+
+			# Keyword Statements
+
+			if   kw[1] == 'while':
+				if not kw[2]: err('SyntaxError: no condition given in while')
+				ladder_id = Literal('3', str(next_ladder_id))
+				snippets.insert('_while_precond', (ladder_id,))
+				size_n = parse(kw[2])
+				snippets.insert('_while_postcond',
+					(ladder_id, Register(str(size_n), 'a')))
+				ladder_stack.append(ladder_id)
+				branch_stack.append(WHILE_BRANCH)
+				next_ladder_id += 1
+				if Shared.debug:
+					print(f'while: {ladder_stack = }, {branch_stack = }')
+			elif kw[1] == 'if':
+				if not kw[2]: err('SyntaxError: no condition given in if')
+				ladder_id = Literal('3', str(next_ladder_id))
+				branch_id = Literal('3', '0')
+				size_n = parse(kw[2])
+				snippets.insert('_if',(
+					ladder_id, branch_id, Register(str(size_n), 'a')))
+				ladder_stack.append(ladder_id)
+				branch_stack.append(branch_id)
+				next_ladder_id += 1
+				if Shared.debug:
+					print(f'if:    {ladder_stack = }, {branch_stack = }')
+			elif kw[1] == 'elif':
+				ladder_id = ladder_stack[-1]
+				branch_id = branch_stack[-1]
+				if branch_id is WHILE_BRANCH:
+					err('SyntaxError: elif not allowed after while')
+				snippets.insert('_else', (ladder_id, branch_id))
+				size_n = parse(kw[2])
+				branch_id.name = str(int(branch_id.name)+1)
+				snippets.insert('_if', (
+					ladder_id, branch_id, Register(str(size_n), 'a')))
+				if Shared.debug:
+					print(f'elif:  {ladder_stack = }, {branch_stack = }')
+			elif kw[1] == 'else':
+				if kw[2]: err('SyntaxError: else takes no expression')
+				ladder_id = ladder_stack[-1]
+				branch_id = branch_stack[-1]
+				if branch_id is WHILE_BRANCH:
+					err('SyntaxError: else not yet supported after while')
+					snippets.insert('_while_else', (ladder_id,))
+				# else: snippets.insert('_if_else', (ladder_id, branch_id))
+				else: snippets.insert('_else', (ladder_id, branch_id))
+				branch_id.name = str(int(branch_id.name)+1)
+				if Shared.debug:
+					print(f'else:  {ladder_stack = }, {branch_stack = }')
+			else:
+				# return, break, continue etc. those that don't end with ':'
+				...
+				output()
+				continue
+			if not kw[3]: err(f'SyntaxError: Colon required at the end of {kw[1]}')
+			expect_indent = True
+
+			output()
+
+		if expect_indent:
+			err('IndentationError: Expected indentation, got EOF instead.')
+		while len(indent_stack) > 1:
+			dedent(indent_stack, branch_stack, ladder_stack)
+
+if __name__ == '__main__':
+	snippets.insert('_header')
+	passes.declaration()
+	passes.codegen()
 	snippets.insert('_exit')
 
 	# print(f'Generated "{output.__kwdefaults__["file"].name}" successfully.')
