285e2147ca92b8e89e89c259d74a27d959cbe4d4
Cubed <47624167+cubed-guy@users.noreply.github.com>
2022-11-24 21:12:17 +0530

    Multiple pointer syntax supports whitespaces
    The assignment isn't supported, but you get a better error message

--- Patterns.py
+++ Patterns.py
@@ -2,13 +2,14 @@ from re import compile as _compile
 wsep  = _compile(r'\b')
 hex   = _compile(r'[\da-fA-F]')
 equal = _compile(r'(?<!=)=(?!=)')
+# equal = _compile(r'((?<!=)|[-+*/&^|%]|<<|>>|//)=(?!=)')
 empty = _compile(r'\[\][3-6]')
 
 _shape = r'(?:(?:\[\d+\])*|\[\]|\**)'
 _unit  = r'(?:[3-6]|[A-Za-z_]\w*)' # add tuple support
 _label = rf'{_shape}{_unit}'
 decl  = _compile(rf'(@({_label})\s+|{_shape}[3-6])([A-Za-z_]\w*)')
-dest  = _compile(rf'(@{_label}\s+|{_shape}[3-6])?(\**)([A-Za-z_]\w*)'
+dest  = _compile(rf'(?:(@{_label}\s+|{_shape}[3-6])|((?:\*\s*)*))?([A-Za-z_]\w*)'
 		r'(?:\[([A-Za-z_]\w*|\d+)\])?')
 token = _compile(rf'@(?P<label>{_label})|(?P<subject>\d+|[a-zA-Z_]\w*)'
 	r'(?:\s*(?:\.\s*(?P<method>[a-zA-Z_]\w*)\s*)?\((?P<args>.*?)\)|'
